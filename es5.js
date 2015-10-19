f075cf3dcdb8144ec796c57d48027c2f577f0634
Nathan LaFreniere <quitlahok@gmail.com>
2015-10-19 13:37:33 -0700

    split out an es5 base config

---
+++ es5.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['eslint-config-hapi'],
+  rules: {
+    "indent": [2, 2],
+    "strict": [2, "global"]
+  }
+};
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 module.exports = {
-  extends: ['eslint-config-hapi'],
+  extends: ['eslint-config-requiresafe/es5'],
   rules: {
-    "indent": [2, 2],
     "no-var": 2,
-    "strict": [2, "global"],
     "arrow-parens": 2,
     "arrow-spacing": 2,
     "prefer-arrow-callback": 2,
