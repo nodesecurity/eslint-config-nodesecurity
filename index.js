98c010f38f9f7d3355f7b4e1562d21996d3b2b80
Nathan LaFreniere <quitlahok@gmail.com>
2015-11-03 07:56:51 -0800

    remove dep on eslint-config-hapi, closes #1

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: ['nodesecurity/es5'],
+  env: {
+    es6: true
+  },
   rules: {
     'no-var': 2,
-    'arrow-parens': 2,
-    'arrow-spacing': 2,
+    'arrow-parens': [2, 'always'],
+    'arrow-spacing': [2, { before: true, after: true }],
+    'hapi/no-arrowception': 2,
     'prefer-arrow-callback': 2,
     'prefer-const': 2,
     'prefer-template': 2,
